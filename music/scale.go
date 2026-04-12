6f1d01649fbdc2cc926c7cab460c4c90358f47b9
Mike Harris <mike@mhbarris.co.uk>
2026-04-12 02:09:03 +0100

    Add helpers for generics etc

---
+++ music/scale.go
@@ -0,0 +1,22 @@
+package music
+
+// Interval is a type constraint satisfied by both JustInterval and TemperedInterval,
+// used by generic functions that operate on either kind of interval.
+type Interval interface {
+	JustInterval | TemperedInterval
+}
+
+// NoteName represents a Western note letter name (A–H, where H is used in German notation).
+// These constants are defined for future use in note-naming functionality.
+type NoteName rune
+
+const (
+	A NoteName = 'A'
+	B NoteName = 'B'
+	C NoteName = 'C'
+	D NoteName = 'D'
+	E NoteName = 'E'
+	F NoteName = 'F'
+	G NoteName = 'G'
+	H NoteName = 'H' // H is used in German/Central European notation (= B♮ in English)
+)
